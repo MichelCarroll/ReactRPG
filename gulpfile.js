3dba9bc0d9b50d850cd46d7c6937d2059b956c1a
Michel Carroll <carrmi.0@gmail.com>
2016-01-13 07:41:27 -0500

    Added a grid of grass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var through2 = require('through2');
+var reactify = require('reactify');
+var babelify = require('babelify');
+
 
 gulp.task('default', function () {
   return gulp.src(['./src/*.js'])
     .pipe(through2.obj(function (file, enc, next) {
         browserify(file.path, { debug: process.env.NODE_ENV === 'development' })
-          .transform(require('reactify'))
-          .transform(require('babelify'))
+          .transform(reactify)
+          .transform(babelify)
           .bundle(function (err, res) {
               if (err) { return next(err); }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+"use strict";
 
 const ReactDOM = require('react-dom');
 const React = require('react');
 
 
-const Test = React.createClass({
+const Grid = React.createClass({
+  getRow: function() {
+    return Array.from(Array(this.props.width).keys()).map((u,i) => <div className="tile grass" key={i}></div>)
+  },
   render: function() {
-    return <h1>Hello World</h1>;
+    return <div className="grid">{
+      Array.from(Array(this.props.height)).map((u,i) => <div className="row" key={i}>{this.getRow()}</div>)
+    }</div>;
   }
 })
 
 
-ReactDOM.render(<Test />, document.getElementById('game'))
+ReactDOM.render(<Grid height={10} width={10} />, document.getElementById('game'))
