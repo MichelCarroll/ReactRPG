519a71289837a02d8ac3d2b86425f1124be80148
Michel Carroll <carrmi.0@gmail.com>
2016-01-13 15:56:41 -0500

    added player to grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,14 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var through2 = require('through2');
-var reactify = require('reactify');
-var babelify = require('babelify');
-
 
 gulp.task('default', function () {
   return gulp.src(['./src/*.js'])
     .pipe(through2.obj(function (file, enc, next) {
         browserify(file.path, { debug: process.env.NODE_ENV === 'development' })
-          .transform(reactify)
-          .transform(babelify)
+          .transform("babelify", {presets: ["es2015", "react"]})
           .bundle(function (err, res) {
               if (err) { return next(err); }
-
               file.contents = res;
               next(null, file);
           });
--- src/index.js
+++ src/index.js
@@ -3,14 +3,39 @@
 const ReactDOM = require('react-dom');
 const React = require('react');
 
+const Player = React.createClass({
+  render: function() {
+    return <div className="player"></div>;
+  }
+});
+
+const Tile = React.createClass({
+  render: function() {
+    return <div className="tile">{
+      React.Children.map(this.props.children, function (child) { return child; })
+    }</div>;
+  }
+});
+
+const Ground = React.createClass({
+  render: function() {
+    return <div className="grass"></div>;
+  }
+});
 
 const Grid = React.createClass({
-  getRow: function() {
-    return Array.from(Array(this.props.width).keys()).map((u,i) => <div className="tile grass" key={i}></div>)
+  getTileContents: function(x, y) {
+    if(x == 5 && y == 5) {
+      return [<Ground key="ground" />, <Player key="player" />];
+    }
+    return [<Ground key={"ground"} />];
+  },
+  getRow: function(y) {
+    return Array.from(Array(this.props.width), (u,x) => <Tile key={x}>{this.getTileContents(x, y)}</Tile>)
   },
   render: function() {
     return <div className="grid">{
-      Array.from(Array(this.props.height)).map((u,i) => <div className="row" key={i}>{this.getRow()}</div>)
+      Array.from(Array(this.props.height), (u,y) => <div className="row" key={y}>{this.getRow(y)}</div>)
     }</div>;
   }
 })
