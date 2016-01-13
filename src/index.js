8ab4e9d1587593d1c4cb1272d94de768430c5107
Michel Carroll <carrmi.0@gmail.com>
2016-01-13 16:14:10 -0500

    allowing player to move

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const ReactDOM = require('react-dom');
 const React = require('react');
+const KeyEvents = require('keyevents');
 
 const Player = React.createClass({
   render: function() {
@@ -11,9 +12,9 @@ const Player = React.createClass({
 
 const Tile = React.createClass({
   render: function() {
-    return <div className="tile">{
+    return <div className="tile"><div className="tile-container">{
       React.Children.map(this.props.children, function (child) { return child; })
-    }</div>;
+    }</div></div>;
   }
 });
 
@@ -24,8 +25,27 @@ const Ground = React.createClass({
 });
 
 const Grid = React.createClass({
+  mixins: [KeyEvents],
+  getInitialState: function() {
+    return {
+      playerX: Math.floor(this.props.width/2),
+      playerY: Math.floor(this.props.height/2)
+    }
+  },
+  componentKeyEvents: function(keypressed) {
+    let action = this.getKeyAction()[keypressed];
+    action && action(keypressed);
+  },
+  getKeyAction: function() {
+    return {
+      up:    () => this.setState({playerY: this.state.playerY - 1}),
+      down:  () => this.setState({playerY: this.state.playerY + 1}),
+      left:  () => this.setState({playerX: this.state.playerX - 1}),
+      right: () => this.setState({playerX: this.state.playerX + 1}),
+    }
+  },
   getTileContents: function(x, y) {
-    if(x == 5 && y == 5) {
+    if(x == this.state.playerX && y == this.state.playerY) {
       return [<Ground key="ground" />, <Player key="player" />];
     }
     return [<Ground key={"ground"} />];
